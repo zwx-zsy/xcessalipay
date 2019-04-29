375ab76baaa819e854668047748eb261c5054950
yancyzhou <python@vip.126.com>
2019-04-29 14:19:20 +0800

    add yaml

--- client/main.go
+++ client/main.go
@@ -8,33 +8,32 @@ import (
 )
 
 // 此处应与服务器端对应
-const address  = "127.0.0.1:8080"
+const address = "xcesspay:28888"
 
 /**
-    1. 创建groc连接器
-    2. 创建grpc客户端,并将连接器赋值给客户端
-    3. 向grpc服务端发起请求
-    4. 获取grpc服务端返回的结果
+  1. 创建groc连接器
+  2. 创建grpc客户端,并将连接器赋值给客户端
+  3. 向grpc服务端发起请求
+  4. 获取grpc服务端返回的结果
 
- */
+*/
 type SomeOne struct {
-	Subject string `json:"subject"`
+	Subject      string `json:"subject"`
 	Out_trade_no string `json:"out_trade_no"`
 	Total_amount string `json:"total_amount"`
 	Product_code string `json:"product_code"`
-	Body string `json:"body"`
-
+	Body         string `json:"body"`
 }
 
 type result struct {
 	AppId string `json:"app_id"`
 }
 
-func main()  {
+func main() {
 
 	// 创建一个grpc连接器
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil{
+	if err != nil {
 		fmt.Println("asdsa")
 		fmt.Println(err)
 	}
@@ -47,32 +46,32 @@ func main()  {
 	//name := &[]pb.AliPayData{}
 
 	// 客户端向grpc服务端发起请求
-	result, err := c.TradePagePay(context.Background(), &pb.AliPayRequest{Mid:"hdkjhsjkf",Data: map[string]string{
-		"NotifyURL":"http://203.86.24.181:3000/alipay",
-		"ReturnURL":"http://liveadmin.cczhou.cn:32080",
-		"Body":"skdyhfiuwehif",
-		"Subject":"测试支付系统-支付宝",
-		"OutTradeNo":"ueoiruwo7981789231",
-		"TotalAmount":"1000.00",
-		"ProductCode":"FAST_INSTANT_TRADE_PAY",
+	result, err := c.TradePagePay(context.Background(), &pb.AliPayRequest{Mid: "hdkjhsjkf", Data: map[string]string{
+		"NotifyURL":   "http://203.86.24.181:3000/alipay",
+		"ReturnURL":   "http://liveadmin.cczhou.cn:32080",
+		"Body":        "skdyhfiuwehif",
+		"Subject":     "测试支付系统-支付宝",
+		"OutTradeNo":  "ueoiruwo7981789231",
+		"TotalAmount": "1000.00",
+		"ProductCode": "FAST_INSTANT_TRADE_PAY",
 	}})
 	response, err := c.TradeRefund(context.Background(), &pb.AliRefundRequest{OutTradeNo: "ueoiruwo7981789231", RefundAmount: "1000.00"})
-	if err !=nil{
-		fmt.Println("shdjkfsd",response)
+	if err != nil {
+		fmt.Println("shdjkfsd", response)
 	}
 	fmt.Println(response)
 	fmt.Println(result)
 	//result, err := c.TradeApp(context.Background(), &pb.AliRequest{Name: "shdjkfsd"})
-	if err != nil{
+	if err != nil {
 		fmt.Println(err.Error())
 		fmt.Println("请求失败!!!")
 		return
 	}
 
 	refoundResponse, err := c.FundTransToAccountTransfer(context.Background(), &pb.AliPayRequest{Data: map[string]string{"OutBizNo": "shgfhdksfasd", "PayeeAccount": "lsvhnj7045@sandbox.com", "Amount": "1.00"}})
-	if err !=nil{
+	if err != nil {
 		fmt.Println(err.Error())
-	}else{
+	} else {
 		fmt.Println(refoundResponse)
 	}
 	// 获取服务端返回的结果
